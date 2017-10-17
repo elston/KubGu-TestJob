d4399e5e3a165e34381f8e56493fd0f6371233dc
Den Elston <4665284@gmail.com>
2017-10-17 15:47:27 +0300

    make_testjob_001_two_dim

--- image/main/src/tz1/app.py
+++ image/main/src/tz1/app.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python
 import logging, argparse
 
-def getsum(i):
-    if len(i) == 1:
-        return 1
+def get_flor(n, m):
+    # ..
+    val_1 =  sum([x for x in range(1,n)])
     # ...
-    return sum(i)
+    return val_1 + m
+
+def get_matrix_item(n, m, o):
+    # ..
+    val_1 =  sum([x**2 for x in range(1,n)])
+    val_2 =  (n * (m - 1))
+    # ..
+    return val_1 + val_2 + o
+
+def get_matrix_x(n, m):
+    return [get_matrix_item(n, m, o) for o in  range(1, n+1)]
+
+def get_matrix_xy(n, room):
+    return [get_matrix_x(n, m) for m in range(1,n+1)]
 
 def main(room):
     # ..
-    x,n = 1,1
-    # while n <= 10:
+    n = 1
+    # while n <= 5:
     while True:        
+        # ..
+        matrix_xy = get_matrix_xy(n, room)
+        # print(n, matrix_xy)
         # ...
-        i = list(range(1,n + 1))
-        p1 = [j-1 for j in range(1,len(i)+1)]
-        p1min = sum(p1)
-        p2 = [j for j in range(1,len(i)+1)]  
-        # ...
-        print(n, [ p1min + item for item in p2] )
-        for m in p2:
-            if room == p1min + m:
-                return (n,m)
+        for i in range(0,n):
+            vi = matrix_xy[i]
+            for j in range(0,len(vi)):
+                vj = vi[j]
+                if vj == room:
+                    return n,i+1,j+1
+        # ..
         n += 1
 
 
@@ -40,8 +54,8 @@ if __name__ == "__main__":
     # ...
     res = main(room=arguments.room)
     if res:
-        n,m = res
-        print(n,m)
+        n,i,j = res
+        print(get_flor(n,i),j)
         exit(0)
     # ..
     print('Fail')
